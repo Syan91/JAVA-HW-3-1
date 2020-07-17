a90ef937a5ffa441d13fe5274a8faf144bd9e689
Syan91 <66880451+Syan91@users.noreply.github.com>
2020-07-17 17:45:31 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 10000;
+        int mile = 20;
+
+        int bonus = price / mile;
+        System.out.println(bonus);
+    }
+}
